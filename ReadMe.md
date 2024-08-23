4b9ff24f7ec2aeeb2cc0a3dc6cba7d4fe450b9d3
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-08-24 00:52:06 +0530

    create db || template inheritance || define model

--- ReadMe.md
+++ ReadMe.md
@@ -6,3 +6,14 @@ Plan :
 - Create a basic CRUD application
 - Deploy to Heroku
 
+# Creating the database: 
+- This is the place the app is going to locate the database: 
+ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' 
+- db = SQLALCHEMY(app)
+- in python shell type: from app import db
+- type: db.create_all()
+- exit()
+
+
+# Concepts Used: 
+- template inheritance
--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__) # create an app instance and this is the main entry point to the application
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # the URI for the database
+db = SQLAlchemy(app) # create a database instance / initialize the database
+
+
+class Todo(db.Model): # create a class for the database
+    id = db.Column(db.Integer, primary_key=True) # the id column
+    content = db.Column(db.String(200), nullable=False) # the content column
+
+    def __repr__(self): # a string representation of the class
+        return '<Task %r>' % self.id
+
 
 @app.route('/') # the site to route to, index/main in this case
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
